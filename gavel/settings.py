39da02f68f069cea31409473b9edab6cbf4d993c
azhan-latif <azhanx@protonmail.com>
2020-03-14 01:12:57 -0400

    Add graphene_django to installed apps. Fix apiserver fix.

--- gavel/settings.py
+++ gavel/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "apiserver.apps.ApiServerConfig",
+    "graphene_django",
+    "apiserver",
     "phonenumber_field"
 ]
 
