90a2be4eb5955e233d4c2cfa1ccfe52104e5ab0a
azhan-latif <azhanx@protonmail.com>
2020-03-14 01:10:13 -0400

    Add GraphQL endpoints

--- gavel/urls.py
+++ gavel/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apiserver.urls'))
+    path("admin/", admin.site.urls),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("", include("apiserver.urls")),
 ]
