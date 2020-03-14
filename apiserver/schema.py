e7bcb8083174c83b54a590c5e895c116ee2f0c9f
azhan-latif <azhanx@protonmail.com>
2020-03-14 16:01:42 -0400

    Add Hearings update and search. Fix court hours update procedure.

--- apiserver/schema.py
+++ apiserver/schema.py
@@ -1,11 +1,11 @@
-import graphene
-from graphene_django import DjangoObjectType
-from graphene_django.converter import convert_django_field_with_choices
+from datetime import date, datetime, timedelta
 
-from .models import (Court, Hearing, Location, OperationalDay, Room, TimeSlot)
-from graphql import GraphQLError
+import graphene
+from django.db.models import Q
+from graphene_django import DjangoObjectType
 
 from .lib.webscrape.ManitobaCourtsScraper import ManitobaCourtsScraper
+from .models import Court, Hearing, Location, OperationalDay, Room, TimeSlot
 
 
 class TimeSlotType(DjangoObjectType):
@@ -87,6 +87,7 @@ class UpdateCourtInfo(graphene.Mutation):
                     # Loop through each timeslot
                     for day in location['address']['hours_of_operations']:
                         time_slots = []
+                        week_day = day['day']
                         for json_timeslot in day['times']:
                             # Create the timeslot if it has not been created
                             time_slot_obj, created = TimeSlot.objects.get_or_create(
@@ -110,13 +111,14 @@ class UpdateCourtInfo(graphene.Mutation):
                         found_day = None
                         # Loop through days to find the correct day with the the exact amount of time slots
                         for day in list(days):
-                            if day.time_slots.count() == len(time_slots):
+                            if (day.time_slots.count() == len(time_slots)
+                                ) and (day.week_day == week_day):
                                 found_day = day
                                 break
                         else:  # Otherwise create a new day the timeslots
                             # Create a new day
                             new_operational_day = OperationalDay(
-                                week_day=day['day'])
+                                week_day=week_day)
                             # Save the new day
                             new_operational_day.save()
                             # Add all the timeslots
@@ -173,19 +175,133 @@ class UpdateCourtInfo(graphene.Mutation):
                                    status_message="Unknown Error Occurred")
 
 
+class UpdateHearings(graphene.Mutation):
+    successful = graphene.Boolean()
+    status_message = graphene.String()
+
+    class Arguments:
+        last_days = graphene.Int()
+        next_days = graphene.Int()
+
+    def mutate(parent, info, last_days=None, next_days=None):
+        scraper = ManitobaCourtsScraper()
+        # Create todays dates
+        back_date = date.today()
+        forward_date = date.today()
+
+        # If parameters given then update dates
+        if last_days:
+            back_date = date.today() - timedelta(days=last_days)
+        if next_days:
+            forward_date = date.today() + timedelta(days=next_days)
+
+        # Loop through each days hearings
+        while back_date <= forward_date:
+            hearings = scraper.scrapeAllCourtHearings(back_date.day,
+                                                      back_date.month,
+                                                      back_date.year)
+            back_date = back_date + timedelta(days=1)
+
+            # Divided first into each court
+            for location in hearings:
+                # Check proper formatting
+                if len(location) == 1:
+                    # Seperate the name an the hearing
+                    location_name: str = ""
+                    hearings = {}
+                    location_name, hearings = list(location.items())[0]
+
+                    # Check if any hearings took place that day in the court
+                    if len(hearings) <= 0:
+                        continue
+
+                    # Remove -QB from the ending
+                    if "-QB" in location_name:
+                        location_name = location_name[:location_name.
+                                                      index("-QB")]
+
+                    # TODO: Add room numbers when available
+                    # Insert each hearing
+                    hearing: Dict = {}
+                    for hearing in hearings:
+                        # TODO: Get room
+
+                        title = hearing.get('title', "")
+                        court_file_number = hearing.get(
+                            'court_file_number', "")
+                        party_name = hearing.get('party_name', "")
+                        date_time = datetime.fromtimestamp(
+                            hearing.get('epoch_time', 0))
+                        lawyer = hearing.get('lawyer', "")
+                        hearing_type = hearing.get('type', "")
+                        # Create and save the object
+                        hearing_obj, created = Hearing.objects.get_or_create(
+                            title=title,
+                            court_file_number=court_file_number,
+                            party_name=party_name,
+                            date_time=date_time,
+                            lawyer=lawyer,
+                            hearing_type=hearing_type)
+                        hearing_obj.save()
+
+        return UpdateHearings(successful=True,
+                              status_message="Successfully updated hearings")
+
+
 class Query(graphene.ObjectType):
     timeslots = graphene.List(TimeSlotType)
-    hearings = graphene.List(HearingType)
+    hearings = graphene.List(HearingType,
+                             file_number=graphene.String(),
+                             date_field=graphene.DateTime(),
+                             party_name=graphene.String(),
+                             title=graphene.String(),
+                             count=graphene.Int())
     rooms = graphene.List(RoomType)
     locations = graphene.List(LocationType)
     operational_days = graphene.List(OperationalDaysType)
-    courts = graphene.List(CourtType)
+    courts = graphene.List(CourtType,
+                           name_search=graphene.String(),
+                           city_search=graphene.String(),
+                           province_search=graphene.String())
 
     def resolve_timeslots(parent, info):
         return TimeSlot.objects.all()
 
-    def resolve_hearings(parent, info):
-        return Hearing.objects.all()
+    def resolve_hearings(parent,
+                         info,
+                         file_number=None,
+                         date_field=None,
+                         party_name=None,
+                         title=None,
+                         count=10):
+        # Check if 1 filter is at least given
+        if (file_number is None) and (date is None) and (
+                party_name is None) and (title is None):
+            return Hearing.objects.order_by('-date_time')[:count]
+
+        all_hearings = Hearing.objects.all()
+        # Filter File number
+        if file_number:
+            all_hearings = all_hearings.filter(
+                Q(court_file_number__icontains=file_number))
+
+        # Filter date
+        if date_field:
+            all_hearings = all_hearings.filter(
+                Q(date_time__year=date_field.year)
+                & Q(date_time__month=date_field.month)
+                & Q(date_time__day=date_field.day))
+
+        # Search party name
+        if party_name:
+            all_hearings = all_hearings.filter(
+                Q(party_name__icontains=party_name))
+
+        # Search title
+        if title:
+            all_hearings = all_hearings.filter(Q(title__icontains=title))
+
+        return all_hearings
 
     def resolve_rooms(parent, info):
         return Room.objects.all()
@@ -196,9 +312,32 @@ class Query(graphene.ObjectType):
     def resolve_operational_days(parent, info):
         return OperationalDay.objects.all()
 
-    def resolve_courts(parent, info):
-        return Court.objects.all()
+    def resolve_courts(parent,
+                       info,
+                       name_search=None,
+                       city_search=None,
+                       province_search=None):
+        # Courts
+        all_courts = Court.objects.all()
+
+        # Filter name
+        if name_search:
+            _filter = Q(name__icontains=name_search)
+            all_courts = all_courts.filter(_filter)
+
+        # In City
+        if city_search:
+            _filter = Q(locations__city__icontains=city_search)
+            all_courts = all_courts.filter(_filter).distinct()
+
+        # Proince search
+        if province_search:
+            _filter = Q(locations__province__icontains=province_search)
+            all_courts = all_courts.filter(_filter).distinct()
+
+        return all_courts
 
 
 class Mutation(graphene.ObjectType):
     update_court_info = UpdateCourtInfo.Field()
+    update_hearings = UpdateHearings.Field()
