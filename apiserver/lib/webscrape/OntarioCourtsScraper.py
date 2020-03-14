e0eee15b9b2f400f627b94a9aa762b8cb85dd5c2
azhan-latif <azhanx@protonmail.com>
2020-03-14 01:09:46 -0400

    Seperate WebScraper class

--- apiserver/lib/webscrape/OntarioCourtsScraper.py
+++ apiserver/lib/webscrape/OntarioCourtsScraper.py
@@ -2,29 +2,10 @@ from ASPNETFormScrapper import ASPNETFormScrapper
 import requests
 import copy
 import sys
-
-class WebScrapper:
-    def __init__(self):
-        self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3835.0 Safari/537.36",
-            "Accept": "*/*",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Accept-Encoding": "gzip, deflate",
-            "DNT": "1",
-            "Connection": "keep-alive",
-            "Pragma": "no-cache",
-            "Cache-Control": "no-cache",
-        }
-        self.baseurl = ""
-
-    def getPostHeaders(self) -> dict:
-        return {
-            **self.headers,
-            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
-        }
+from .WebScraper import WebScraper
 
 
-class OntarioCourtsScraper(WebScrapper):
+class OntarioCourtsScraper(WebScraper):
     def __init__(self):
         super().__init__()
         self.baseurl = "http://www.ontariocourtdates.ca/"
