fc63c786a1c2b03084b45d18e1cdc0be281bd01e
azhan-latif <azhanx@protonmail.com>
2020-03-22 14:48:22 -0400

    Reorganzie update to minimize downtime.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-cd ..
 
 pipenv shell
 
-su - root -c "pkill -f uwsgi -9"
+cd ..
 
+echo "Pulling updates..."
 git pull
 
 ./manage.py makemigrations
 ./manage.py migrate
+
+echo "Please enter root password to kill old processes..."
+su - root -c "pkill -f uwsgi -9"
 
 uwsgi --yaml uwsgi.yaml &
 disown
